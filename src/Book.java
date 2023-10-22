32b2f82bb1e4d624390efcdc9fef1ae31a57f129
Rabota <wulf1205@gmail.com>
2023-10-22 14:36:34 +0700

    HW на проверку

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,25 @@
 public class Book {
+    private String name;
+    private String author;
+    private int age;
+
+    public Book(String name, String author, int age) {
+        this.name = name;
+        this.author = author;
+        this.age = age;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book = new Book("Мастер и маргарита", "М. Булгаков", 1929);
+        book.setAge(1940);
+        System.out.println("Название: "+book.getName());
+        System.out.println("Автор:"+book.getAuthor());
+        System.out.println("Год издания:"+book.getAge());
+
+
+        Book book1 = new Book("Записки из Мертвого дома", "Ф.Достоевский",1860);
+        System.out.println("Название: "+book1.getName());
+        System.out.println("Автор: "+book1.getAuthor());
+        System.out.println("Год издания: "+book1.getAge());
+        book1.setAge(1961);
+        System.out.println("Год переиздания " + book1.getAge());
     }
 }
