504c517096cb0bcd3a8f2f6bd39eea3023edbaa3
Rabota <wulf1205@gmail.com>
2023-10-25 15:18:55 +0700

    Переделал

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ public class Main {
         Author author = new Author("Булгаков ","Михаил");
         Book book = new Book("Мастер и маргарита",1929);
         book.setAge(1940);
-        System.out.println("Название: "+book.getTitle());
-        System.out.println("Автор:"+author.getSurname());
-        System.out.println("Автор:"+author.getName());
-        System.out.println("Год издания:"+book.getAge());
+        System.out.println("Название: "+book.getTitle()+" Год издания: "+book.getAge());
+        System.out.println("Автор:"+author.getSurname() +author.getName());
+
+
 
 
         Author author1 = new Author("Достоевский", "Федор");
         Book book1 = new Book("Записки из Мертвого дома",1860);
-        System.out.println("Название: "+book1.getTitle());
-        System.out.println("Автор: "+author1.getSurname());
-        System.out.println("Автор: "+author1.getName());
-        System.out.println("Год издания: "+book1.getAge());
+        System.out.println("Название: "+book1.getTitle()+" Год издания: "+book1.getAge());
+        System.out.println("Автор: "+author1.getSurname()+author1.getName());
         book1.setAge(1961);
         System.out.println("Год переиздания " + book1.getAge());
     }
